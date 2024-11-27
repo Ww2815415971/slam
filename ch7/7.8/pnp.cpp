16ad8f8fa161adf2760154aa00fc3d20d3271ee7
Ww2815415971 <2815415971@qq.com>
2024-11-27 20:07:18 +0800

    pnp

---
+++ ch7/7.8/pnp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <Eigen/Core>
+#include <g2o/core/base_unary_edge.h>
+#include <g2o/core/sparse_optimizer.h>
+#include <g2o/core/solver.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+#include <g2o/core/base_vertex.h>
+#include <sophus/se3.hpp>
+#include <chrono>
+using namespace std;
+int main()
+{
+    printf("123");
+}
