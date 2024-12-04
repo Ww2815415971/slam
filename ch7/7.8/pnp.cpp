b2315109fc03bbb351d21d38280db16f92b70302
Ww2815415971 <2815415971@qq.com>
2024-12-04 17:34:32 +0800

    pnp

--- ch7/7.8/pnp.cpp
+++ ch7/7.8/pnp.cpp
@@ -7,6 +7,7 @@
 #include <g2o/core/base_unary_edge.h>
 #include <g2o/core/sparse_optimizer.h>
 #include <g2o/core/solver.h>
+#include <g2o/core/block_solver.h>
 #include <g2o/core/optimization_algorithm_gauss_newton.h>
 #include <g2o/solvers/dense/linear_solver_dense.h>
 #include <g2o/core/base_vertex.h>
@@ -159,6 +160,118 @@ void bundleAdjustmentGaussNewton(
         if(isnan(dx[0]))
         {
             cout << "result is nan!" << endl;
+            break;
+        }
+        if(iter > 0 && cost >= lastCost)
+        {
+            cout << "cost: "<<cost <<", last cost:" << lastCost;
+            break;
+        }
+        pose = Sophus::SE3d::exp(dx) *pose;
+        lastCost = cost;
+
+        cout << "iteration" << iter << "cost = " << std::setprecision(12) << cost<<endl;
+        // 导数值为0；
+        if(dx.norm() < 1e-6)
+        {
+            break;
         }
     }
+        cout << "pose by g-n: \n" <<pose.matrix()<< endl;
+
+    }
+    class VertexPose : public g2o::BaseVertex<6 , Sophus::SE3d>{
+    public:
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+      // 提高效率
+        //去进行在图优化时结点进行初始化，改为默认值单位矩阵，无平移无旋转
+        virtual void setToOriginImpl()override {
+            _estimate = Sophus::SE3d();
+        }
+        virtual void oplusImpl(const double *update) override
+        {
+            Eigen::Matrix<double,6,1> update_eigen;
+            update_eigen << update[0],update[1],update[2],update[3],update[4],update[5];
+           // 将更新数组转化为Eigen矩阵形式 
+            _estimate = Sophus::SE3d::exp(update_eigen) * _estimate;
+            // 将群映射到指数映射并去使用群乘法去进行更新，隐含了雅可比矩阵在里面
+        }
+        //读入和读出顶点的数据
+        virtual bool read(istream &in) override{}
+
+        virtual bool write(ostream &out) const override{}
+    };
+
+class EdgeProjection : public g2o::BaseUnaryEdge<2,Eigen::Vector2d,VertexPose>
+{
+    public:
+
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+        EdgeProjection(const Eigen::Vector3d &pos,const Eigen::Matrix3d &K): _pos3d(pos),_K(K)
+        {}
+
+        virtual void computeError() override
+        {
+            const VertexPose *v =static_cast<VertexPose *> (_vertices[0]);
+            Sophus::SE3d T = v->estimate();
+            Eigen::Vector3d pos_pixel = _K *(T * _pos3d);
+            pos_pixel /= pos_pixel[2];
+            _error = _measurement - pos_pixel.head<2>(); 
+        }
+        virtual void linearizeOplus() override{
+            const VertexPose *v = static_cast<VertexPose *> (_vertices[0]);
+            Sophus::SE3d T = v->estimate();
+            Eigen::Vector3d pos_cam = T*_pos3d;
+            double fx = _K(0,0);
+            double fy = _K(1,1);
+            double cx = _K(0,2);
+            double cy = _K(1,2);
+            double X = pos_cam[0];
+            double Y = pos_cam[1];
+            double Z = pos_cam[2];
+            double Z2 = Z * Z;
+            _jacobianOplusXi
+            << -fx/Z, 0, fx * X / Z2, fx * X *Y /Z2, -fx -fx * X * X /Z2,fx * Y /Z,
+            0, -fy / Z, fy * Y /(Z *Z), fy + fy * Y * Y /Z2, -fy * X * Y / Z2, -fy * X /Z;
+
+        }
+        virtual bool read(istream &in) override {}
+        virtual bool write(ostream &out) const override{}
+
+    private:
+     Eigen:: Vector3d _pos3d;
+     Eigen::Matrix3d _K;
+};
+
+void bundleAdjustmentG2O(
+    const VecVector3d &points_3d,
+    const VecVector2d &points_2d,
+    const Mat &K,
+    Sophus::SE3d &pose)
+{
+        // 构建图优化 设定g2o
+
+        // g2o 的pose is 6 landmark is 3 
+        // 位姿是 6 维的包括相机的位置xyz 和Z: Yaw（绕 Z 轴旋转）。
+        // Y: Pitch（绕 Y 轴旋转）。 当为+-90时 会导致万向锁，即丢失自由度
+        // X: Roll（绕 X 轴旋转）   
+        //和姿态 路标是 3 维的 xyz
+        typedef g2o::BlockSolver<g2o::BlockSolverTraits<6, 3>> BlockSolverType; 
+        // 线性求解器
+        typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType;
+
+        // 梯度下降方法
+        // config 配置求解器，初始化图模型，打开调试输出
+        auto solver = new g2o::OptimizationAlgorithmGaussNewton(
+            std::make_unique<BlockSolverType>(std::make_unique<LinearSolverType>()));
+            g2o::SparseOptimizer optimizer; // 图模型
+            optimizer.setAlgorithm(solver); // 设置求解器
+            optimizer.setVerbose(true);    // 打开调试输出
+
+        // vertex init
+
+        VertexPose *vertex_pose = new VertexPose();
+        
+        
 }
